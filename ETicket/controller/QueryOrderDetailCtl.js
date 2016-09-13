4547ca8e8f4b6f01733d4242c9806c64a7b1dcc5
Jerry <jerry@Jerrymac.local>
2016-09-13 10:04:29 +0800

    导航栏css高亮调整

--- ETicket/controller/QueryOrderDetailCtl.js
+++ ETicket/controller/QueryOrderDetailCtl.js
@@ -10,7 +10,7 @@ laAir.controller('laAir_ETicket_QueryOrderDetailPageCtl', ['$window', '$document
      * 设置导航栏ClassName
      * @type {boolean}
      */
-    $scope.isQueryOrderNav = true;
+    $scope.isSchTripNav = true;
 
     $scope.orderId;
     $scope.sessionId;
